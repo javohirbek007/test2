5702c6c4403ff061af3c7507a570770d0d716281
User <javohirbek007>
2022-05-23 19:07:25 +0300

    make it better

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -2,8 +2,6 @@ from aiogram import Dispatcher
 
 from loader import dp
 from .throttling import ThrottlingMiddleware
-# from .azolikni_tekshirish import Asosiy_chesking
 
 if __name__ == "middlewares":
     dp.middleware.setup(ThrottlingMiddleware())
-    # dp.middleware.setup(Asosiy_chesking())
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from . import db_api
 from . import misc
-# from . import majburiy_azolik
+
 from .notify_admins import on_startup_notify
