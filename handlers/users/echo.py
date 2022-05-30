6a0ae2d84bfeb599349e412abcd0f324396c9fb3
User <javohirbek007>
2022-05-30 15:38:32 +0300

    make it better

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -6,4 +6,4 @@ from loader import dp
 # Echo bot
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer('bunday buyruq yoq')
+    await message.answer('xato')
