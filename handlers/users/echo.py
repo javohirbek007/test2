fdb405b449c3fcea65c9a4fe1e2d1a6c90b49c61
User <javohirbek007>
2022-06-08 18:13:56 +0300

    make it better

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,9 +1,9 @@
 from aiogram import types
 
 from loader import dp
-
+from keyboards.inline.menu import inline_menu
 
 # Echo bot
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer('xato')
+    await message.answer(message.text,reply_markup=inline_menu)
